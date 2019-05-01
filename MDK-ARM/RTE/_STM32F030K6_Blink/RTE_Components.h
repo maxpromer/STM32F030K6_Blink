873f9a174c1a5cec0d6137002b651b27f065e97d
Max <max30012540@hotmail.com>
2019-05-01 20:54:29 +0700

    Initial commit

---
+++ MDK-ARM/RTE/_STM32F030K6_Blink/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32F030K6_Blink' 
+ * Target:  'STM32F030K6_Blink' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/STM32F030K6_Blink/startup_stm32f030x6.d
@@ -0,0 +1,1 @@
+stm32f030k6_blink\startup_stm32f030x6.o: startup_stm32f030x6.s
